7c0cfa6a43eee321ddcc3dbcb4f4fa5f6d02ce73
Mykhailo Bondarenko <mikhailzyagun@gmail.com>
2022-06-30 11:16:22 +0300

    add 3 files

---
+++ main.js
@@ -0,0 +1,43 @@
+const input = document.querySelector('.main__input');
+const button = document.querySelector('.main__button');
+const result = document.querySelector('.main__result');
+
+button.addEventListener('click', getValue);
+
+function getValue() {
+  const value = input.value;
+  const array = value.split('');
+  let innerHTML = '';
+  for (let i = 0; i < array.length; i++) {
+    innerHTML += `<div class="value" draggable="true">${array[i]}</div>`;
+    result.innerHTML = innerHTML;
+  }
+
+  const values = document.querySelectorAll('.value');
+  let offsetX = null;
+  let offsetY = null;
+  let current;
+
+  values.forEach(function (value) {
+    value.addEventListener('dragstart', function (event) {
+      current = this;
+      offsetX = event.offsetX;
+      offsetY = event.offsetY;
+    })
+    value.addEventListener('dragend', function (event) {
+      current.style.position = 'absolute';
+      current.style.top = (event.pageY - offsetY) + 'px';
+      current.style.left = (event.pageX - offsetX) + 'px';
+    });
+  })
+}
+
+
+
+
+
+
+
+
+
+
